f8bdd1f5e54f4eda484225e5dc7352ce9b21ef4b
Inyoung <eyewhy92@gmail.com>
2024-03-17 02:28:52 +0900

    240317 api 연동

---
+++ react-test/api-test/src/App.js
@@ -0,0 +1,69 @@
+/*
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/todos/1'
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
+};
+
+export default App;
+*/
+
+/*
+import Newslist from '. /components/Newslist ' ; 
+import Categories from './components/Categories ' ; 
+
+    
+  const App = () => {
+      return (
+        <>
+          <Categories />
+          <Newslist />
+        </>
+      );
+  }
+
+export default App; 
+*/
+
+import React, { useState, useCallback } from 'react';
+import NewsList from './components/NewsList';
+import Categories from './components/Categories';
+
+const App = () => {
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback((category) => setCategory(category), []);
+
+  return (
+    <>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
+    </>
+  );
+};
+
+export default App;
